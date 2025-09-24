b4276fbbc3275950e18eaaed840f8cdc689822c4
yellow-cube <93196588+yellow-cube@users.noreply.github.com>
2025-09-24 14:15:51 +0300

    Update QUERIO_SE_CANISTER_ID

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-export const QUERIO_SE_CANISTER_ID =  'cru6d-taaaa-aaaan-qmz6q-cai';
+export const QUERIO_SE_CANISTER_ID =  'vpcty-kqaaa-aaaan-qz6xa-cai';
 export const QUERIO_SE_HOST = 'https://icp0.io';
